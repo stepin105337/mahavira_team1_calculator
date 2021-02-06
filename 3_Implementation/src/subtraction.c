0bfd22cbe00c8e09e8422b70f3b677b0fa71b46d
99003582 <78539923+99003582@users.noreply.github.com>
2021-02-06 09:12:35 +0530

    Add files via upload

---
+++ 3_Implementation/src/subtraction.c
@@ -0,0 +1,11 @@
+#include "subtraction.h"
+void subtraction(){
+
+    int a,b,diff;
+    printf("enter input 1:");
+    scanf("%d",&a);
+     printf("enter input 2:");
+    scanf("%d",&b);
+     diff=a-b;
+     printf("tha output of subtraction is :%d",sum);
+}
